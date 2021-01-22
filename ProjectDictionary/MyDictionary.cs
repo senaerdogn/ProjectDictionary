b1c919c882e0d2acdb9b90c59b364d428b828fd5
c1411019 <c1411019@student.cankaya.edu.tr>
2021-01-22 22:44:19 +0300

    Add project files.

---
+++ ProjectDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectDictionary
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] _keys, _tempKeys;
+        Tvalue[] _values, _tempValues;
+
+        public MyDictionary()
+        {
+            _keys = new Tkey[0];
+            _values = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            _tempKeys = _keys;
+            _tempValues = _values;
+
+            _keys = new Tkey[_tempKeys.Length + 1];
+            _values = new Tvalue[_tempValues.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _keys[i] = _tempKeys[i];
+                _values[i] = _tempValues[i];
+            }
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return _keys.Length; }
+        }
+
+        public Tkey[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public Tvalue[] Values
+        {
+            get { return _values; }
+        }
+    }
+}
---
+++ ProjectDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjectDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Adana");
+            myDictionary.Add(2, "Adıyaman");
+            myDictionary.Add(3, "Afyon");
+            myDictionary.Add(4, "Ağrı");
+            myDictionary.Add(5, "Amasya");
+            myDictionary.Add(6, "Ankara");
+
+            Console.WriteLine("Dictionary Size: " + myDictionary.Count);
+
+            for (int i = 0; i < myDictionary.Count; i++)
+            {
+                Console.WriteLine(myDictionary.Keys[i] + " " + myDictionary.Values[i]);
+            } 
+        }
+    }
+}
